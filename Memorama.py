6803894a6dd1e3a5533a1b1be97df4f6fdaedcbb
Pablo Ricardo Duran <pabloricardoduran2003@gmail.com>
2023-05-11 20:10:31 -0600

    Act5 paso 5, se agrego un contador, se cambiaron los numeros por caracteres y se centraron

--- Memorama.py
+++ Memorama.py
@@ -1,27 +1,21 @@
-"""Memory, puzzle game of number pairs.
-
-Exercises:
-
-1. Count and print how many taps occur.
-2. Decrease the number of tiles to a 4x4 grid.
-3. Detect when all tiles are revealed.
-4. Center single-digit tile.
-5. Use letters instead of tiles.
-"""
-
 from random import *
 from turtle import *
-
 from freegames import path
 
 car = path('car.gif')
-tiles = list(range(32)) * 2
+
+# Semodifica los caracteres del arreglo, tiene que ser 32
+tiles = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z', 'W',
+         '@', '#', '$', '%', '&', '*'] * 2
 state = {'mark': None}
 hide = [True] * 64
 
+# Definimos el contador
+touch_count = 0
+
 
 def square(x, y):
-    """Draw white square with black outline at (x, y)."""
+    "Draw white square with black outline at (x, y)."
     up()
     goto(x, y)
     down()
@@ -34,17 +28,20 @@ def square(x, y):
 
 
 def index(x, y):
-    """Convert (x, y) coordinates to tiles index."""
+    "Convert (x, y) coordinates to tiles index."
     return int((x + 200) // 50 + ((y + 200) // 50) * 8)
 
 
 def xy(count):
-    """Convert tiles count to (x, y) coordinates."""
+    "Convert tiles count to (x, y) coordinates."
     return (count % 8) * 50 - 200, (count // 8) * 50 - 200
 
 
 def tap(x, y):
-    """Update mark and hidden tiles based on tap."""
+    # Actualizar la función tap() para incrementar el contador y mostrar el resultado
+    global touch_count
+    touch_count += 1
+
     spot = index(x, y)
     mark = state['mark']
 
@@ -55,9 +52,14 @@ def tap(x, y):
         hide[mark] = False
         state['mark'] = None
 
+    # Modifcar el contador cada vez
+    counter_turtle.clear()
+    counter_turtle.write("Contador: {}".format(touch_count),
+                         align='left', font=('Arial', 12, 'normal'))
+
 
 def draw():
-    """Draw image and tiles."""
+    "Draw image and tiles."
     clear()
     goto(0, 0)
     shape(car)
@@ -71,15 +73,24 @@ def draw():
     mark = state['mark']
 
     if mark is not None and hide[mark]:
+        5
         x, y = xy(mark)
         up()
-        goto(x + 2, y)
+        goto(x+25, y+4)  # Ajuste de posición para centrar el número
         color('black')
-        write(tiles[mark], font=('Arial', 30, 'normal'))
+        write(tiles[mark], align='center', font=('Arial', 30, 'normal'))
 
     update()
     ontimer(draw, 100)
 
+
+# Crear el contador en la pantalla
+counter_turtle = Turtle()
+counter_turtle.hideturtle()
+counter_turtle.up()
+# Ajustar la posición del contador en la pantalla
+counter_turtle.goto(-190, 200)
+counter_turtle.write("Contador: 0", align='left', font=('Arial', 12, 'normal'))
 
 shuffle(tiles)
 setup(420, 420, 370, 0)
